f21b97675b29217fe5211d379ca9e5ba5afbd6a1
arkadianriver <arkadianriver@gmail.com>
2017-06-17 00:17:52 -0700

    nospampleez

---
+++ mailtome.php
@@ -0,0 +1,38 @@
+---
+---
+<?php
+/**
+ * Captcha bit from http://99webtools.com/blog/php-simple-captcha-script/
+ */
+session_start();
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+  header($_SERVER["SERVER_PROTOCOL"] ." 404 Not Found");
+} else {
+  if ( isset($_POST["captcha"])
+          && $_POST["captcha"] != ""
+          && $_SESSION["code"] == $_POST["captcha"]
+     ) {
+    $name = $_POST["name"] ? $_POST["name"] : '[no name given]';
+    $msg = <<<EOM
+From your arkadianriver.com website...
+
+Mail received from $name, at {$_POST["email"]}.
+
+{$_POST["textbody"]}
+EOM;
+    $headers = 'From: {{ site.data.tokens.fromaddy }}'."\r\n".
+               'X-Mailer: PHP/'. phpversion();
+    $rc = mail('{{ site.data.tokens.toaddy }}',
+               $_POST["subject"],
+               $msg, $headers);
+    if ($rc) {
+      header('Location: '. $_POST["success"]);
+    } else {
+      header('Location: '. $_POST["failure"]);
+    }
+  } else {
+    die("Wrong Captcha code entered");
+  }
+}
+
+?>
